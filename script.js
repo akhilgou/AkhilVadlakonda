6a49b90a7b9b73508d760c395d1bed2e4f6f6669
akhilgou <vakhilgoud@gmail.com>
2025-11-16 11:15:21 -0500

    Add smooth scrolling functionality

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
