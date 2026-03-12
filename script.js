1a529b80a992b3e4408e079215a10bef5ac62895
akhilgou <vakhilgoud@gmail.com>
2026-03-11 21:42:57 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
